537ec3ae356d555194da45ad6a8819bb700173ac
Arnau <arnauph06@gmail.com>
2026-04-13 17:12:42 +0200

    Proyecto y clases iniciadas

---
+++ tienda-jddl-aph-web/src/main/java/es/iesclaradelrey/da2d1a/tiendajddlaph/web/TiendaJddlAphWebApplication.java
@@ -0,0 +1,13 @@
+package es.iesclaradelrey.da2d1a.tiendajddlaph.web;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TiendaJddlAphWebApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TiendaJddlAphWebApplication.class, args);
+    }
+
+}
